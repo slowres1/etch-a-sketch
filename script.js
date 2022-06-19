d2d5f077ee945dbbbfc75fd28e1f31c230be3bab
Stanley Lowres <slowres@gmail.com>
2022-06-19 22:24:53 +0100

    mouseover turns squares blue

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const container = document.querySelector('.container');
+let verticalDiv;
 
 for (let i=0; i<16; i++) {
     const horizontalDiv = document.createElement('div');
     horizontalDiv.classList.add('horizontal');
     for (let j=0;j<16;j++) {
-        const verticalDiv = document.createElement('div');
+        verticalDiv = document.createElement('div');
         verticalDiv.classList.add('vertical');
+
         horizontalDiv.appendChild(verticalDiv);
     }
     container.appendChild(horizontalDiv);
 }
+
+const pixels = document.querySelectorAll('.vertical');
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.classList.add('mouseover');
+    });
+});
