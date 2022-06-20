06d58dd064f2c7253cb71ee7ad46e39b06f6aa90
Stanley Lowres <slowres@gmail.com>
2022-06-20 09:15:04 +0100

    add option to change size of grid/number of boxes

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-const container = document.querySelector('.container');
-let verticalDiv;
-
-for (let i=0; i<16; i++) {
-    const horizontalDiv = document.createElement('div');
-    horizontalDiv.classList.add('horizontal');
-    for (let j=0;j<16;j++) {
-        verticalDiv = document.createElement('div');
-        verticalDiv.classList.add('vertical');
-
-        horizontalDiv.appendChild(verticalDiv);
+function createGrid (gridSize) {
+    for (let i=0; i<gridSize; i++) {
+        const horizontalDiv = document.createElement('div');
+        horizontalDiv.classList.add('horizontal');
+        for (let j=0;j<gridSize;j++) {
+            verticalDiv = document.createElement('div');
+            verticalDiv.classList.add('vertical');
+    
+            horizontalDiv.appendChild(verticalDiv);
+        }
+        container.appendChild(horizontalDiv);
     }
-    container.appendChild(horizontalDiv);
-}
-
-const pixels = document.querySelectorAll('.vertical');
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', () => {
+    const pixels = document.querySelectorAll('.vertical');
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', () => {
         pixel.classList.add('mouseover');
     });
 });
+}
+
+function getNumber() {
+    let userInput = parseInt(prompt('Enter a grid size:'));
+    if (userInput) {
+        //alert(`You have selected: ${userInput}.`)
+        return userInput;
+    } else {
+        alert('Please enter a valid number');
+    }
+};
+
+const container = document.querySelector('.container');
+let verticalDiv;
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let gridSize;
+    while (!gridSize) {
+        gridSize = getNumber();
+    }
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    createGrid(gridSize);
+})
+
+
+createGrid(16);
+
+
+
